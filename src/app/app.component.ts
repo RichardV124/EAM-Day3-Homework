d219e4f87cdbf3efab00062d39bb0adc7fae6c8d
Richard Vanegas <richardvanegas8@hotmail.com>
2018-04-07 12:23:22 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	url = 'https://yesno.wtf/api';
+	answer;
+
+	constructor(private http: HttpClient) {
+		this.answer = http.get(this.url);
+	}
 }
